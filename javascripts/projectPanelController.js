76ca186c96512570519424a3ae561d834ec5d89c
Guohao Yan <guohao.yan@mail.utoronto.ca>
2016-05-13 10:41:58 -0400

    styling

--- javascripts/projectPanelController.js
+++ javascripts/projectPanelController.js
@@ -1,7 +1,7 @@
 angular.module('projectPanel', []).controller('projectPanelController', function($scope) {
     var imagePath = "resources/images/"
     $scope.projects = [{
-        "name": "JavaFX-PasswordManager",
+        "name": "JavaFX PasswordManager",
         "img": imagePath + "javaFX.png",
         "link": "https://github.com/sccdmaomao/JavaFX-PasswordManager"
     }];

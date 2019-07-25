118b6eee07a8ba0a2071b7e5a9647d1c255b8d0f
sccdmaomao <sccdmaomao@hotmail.com>
2019-07-25 17:01:27 -0400

    Publish - HashedBrowser history

--- source/src/store/initApp.ts
+++ source/src/store/initApp.ts
@@ -1,11 +1,11 @@
 import { connectRouter, routerMiddleware } from 'connected-react-router'
-import { createBrowserHistory } from 'history'
+import { createHashHistory } from 'history'
 import { loadingBarReducer } from 'react-redux-loading-bar'
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { reducer as formReducer } from 'redux-form'
 
-export const history = createBrowserHistory()
+export const history = createHashHistory()
 const reducers = {
   router: connectRouter(history),
   form: formReducer,

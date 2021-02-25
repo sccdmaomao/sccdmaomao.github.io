fbdf8cf78eb857332ed9300e9cce889789ea1971
Guohao Yan <sccdmaomao@hotmail.com>
2021-02-25 01:05:03 -0500

    test circle ci auto deloy

--- src/texts/Biography/index.ts
+++ src/texts/Biography/index.ts
@@ -1,8 +1,4 @@
 export const title = ''
 
 export const bodyText = `
-Welcome, this is Guohao, the creator of this page. 
-I graduated from UofT couple years ago and currently employed as a software engineer.
-I do a lot of things on my free time, sports, gaming, programming my small ideas.
-I publish some of my achievements here, feel free to browse around.
-`
+Welcome, this is Guohao, the creator of this page. test`
